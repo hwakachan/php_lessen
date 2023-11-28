334c989992da4f8cd9360993cc2d1ffc6bfb3251
hwakachan <hi_wakachan0823@icloud.com>
2023-11-29 00:08:36 +0900

    first commit

---
+++ test/if_sample.php
@@ -0,0 +1,10 @@
+<?php
+$result = 90;
+
+if ($result > 80) {
+    echo '合格';
+} elseif ($result > 60) {
+    echo '惜しい';
+} else {
+    echo '残念';
+}
---
+++ test/test.php
@@ -0,0 +1,4 @@
+<?php
+$test = 'hello';
+echo $test;
+?>
